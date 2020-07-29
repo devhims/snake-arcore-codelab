bcef0b7e8195a703fd04e921d72991a466f3cbc0
DeekshitaV <deekshitaverma@gmail.com>
2020-07-29 10:53:21 +0530

    OptimizedTheUI
    Added a new font and a welcome scene separately

---
+++ Assets/Welcome.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Welcome : MonoBehaviour
+{
+    public void BtnStartGame()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+}
